64ed34434eccb9a95fc3e9e63f5de6e4a1254015
Susan Cheng <susan.doggie@gmail.com>
2022-10-22 21:12:26 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
-import sass from 'rollup-plugin-sass'
+import sass from 'rollup-plugin-sass';
 import dts from 'rollup-plugin-dts';
 
 const rollupPlugins = [
